71f7cc6f377cbe054bb45f72384e7dff891b07ed
nnuyz <hellonedesing@gmail.com>
2023-08-10 02:37:17 +0300

    add header

--- app.vue
+++ app.vue
@@ -2,10 +2,8 @@
 import '@unocss/reset/tailwind.css'
 </script>
 <template>
+  <CoreCursor />
   <div class="min-h-screen bg-white flex flex-col">
-    <div class="w-container text-black text-5xl font-body-extrabold">
-      Привет меня зовут Сергей Филимошкин и я
-      <span class="font-mak-bold text-blue-7">frontend-developer</span>
-    </div>
+    <GlobalHeader />
   </div>
 </template>
---
+++ components/global/header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="fixed w-full top-0 left-0 flex justify-between items-center pt-30px">
+    <div class="w-container flex justify-between items-center">
+      <nuxt-link to="/" class="text-accent-purple font-mak font-bold">Logotype</nuxt-link>
+      <nav
+        class="flex flex-col gap-8px text-14px leading-20px text-accent-dark relative overflow-hidden"
+      >
+        <a
+          v-for="([name, link], index) in links"
+          :key="index"
+          :href="link"
+          :data-after="name"
+          class="nav-links"
+        >
+          <span>
+            {{ name }}
+          </span>
+        </a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+const links = [
+  ['Обо мне', '#about'],
+  ['Проекты', '#work'],
+  ['Контакты', '#contacts'],
+]
+</script>
+
+<style lang="postcss">
+.nav-links {
+  @apply relative p-0 overflow-hidden flex after:content-[attr(data-after)] after:block after:text-accent-purple after:translate-y-full after:absolute after:left-0 after:transition-inherit;
+  transition: transform 0.3s;
+  span {
+    @apply translate-y-0 transition-inherit;
+  }
+  &:hover {
+    span {
+      @apply -translate-y-full;
+    }
+    &::after {
+      @apply translate-y-0;
+    }
+  }
+}
+</style>
