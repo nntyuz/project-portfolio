1d5b01db0b242779bc275f852c024c7bcbfbdaff
nnuyz <hellonedesing@gmail.com>
2023-08-16 02:05:23 +0300

    init page

--- components/home/showreal.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <section class="showreal-section padding-1" id="projects">
-    <video muted autoplay loop>
-      <source src="/showreal.mp4" type="video/mp4" />
-    </video>
-  </section>
-</template>
