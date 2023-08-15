99fb70c4d39dc39abf26c43492aff2eb3a02fa42
nnuyz <hellonedesing@gmail.com>
2023-08-16 01:44:11 +0300

    add showreal

---
+++ components/home/showreal.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="showreal-section padding-1" id="projects">
+    <video muted autoplay loop>
+      <source src="/showreal.mp4" type="video/mp4" />
+    </video>
+  </section>
+</template>
