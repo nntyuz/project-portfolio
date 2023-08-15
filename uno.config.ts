726de6edb6186fe82ab5802ba26af46b7ee1913a
nnuyz <hellonedesing@gmail.com>
2023-08-16 01:39:56 +0300

    add colors

--- uno.config.ts
+++ uno.config.ts
@@ -26,12 +26,14 @@ export default defineConfig({
     },
     colors: {
       'accent-dark': '#262626',
-      'accent-light': '#f1f1f1',
+      'accent-light': '#fff',
       'accent-blue': '#001BEB',
       'accent-red': '#FF695E',
       'accent-yellow': '#F9A826',
       'accent-purple': '#6C63FF',
       'accent-sand': '#b7ab98',
+      'accent-emerald': '#50c878',
+      'accent-pink': '#ffc0cb',
     },
     fontFamily: {
       mak: ['"Mak"', 'Arial', 'Helvetica', 'sans-serif'],
