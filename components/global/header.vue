930215dae990c70f628c31d4689bce14116b60d8
nnuyz <hellonedesing@gmail.com>
2023-08-16 01:42:39 +0300

    add header

--- components/global/header.vue
+++ components/global/header.vue
@@ -1,9 +1,36 @@
+<script setup>
+const links = [
+  ['Обо мне', '#about'],
+  ['Проекты', '#projects'],
+  ['Контакты', '#contacts'],
+]
+const switchValue = ref(false)
+const toggleTheme = () => {
+  document.documentElement.classList.toggle('dark')
+
+  if (document.documentElement.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark')
+  } else {
+    localStorage.setItem('theme', 'light')
+  }
+}
+onMounted(() => {
+  if (localStorage.getItem('theme') === 'dark') {
+    switchValue.value = false
+  } else {
+    switchValue.value = true
+  }
+})
+</script>
+
 <template>
-  <header class="fixed w-full top-0 left-0 flex justify-between items-center pt-30px">
+  <header
+    class="section-header fixed w-full top-0 left-0 flex justify-between items-center py-5 bg-accent-light z-12 dark:bg-accent-dark"
+  >
     <div class="w-container flex justify-between items-center">
-      <nuxt-link to="/" class="text-accent-purple font-mak font-bold">Logotype</nuxt-link>
+      <nuxt-link to="/" class="text-accent text-14px leading-20px">Maria Anikina</nuxt-link>
       <nav
-        class="flex flex-col gap-8px text-14px leading-20px text-accent-dark relative overflow-hidden"
+        class="w-max flex gap-40px text-14px leading-20px text-accent-dark relative overflow-hidden dark:text-accent-light"
       >
         <a
           v-for="([name, link], index) in links"
@@ -17,19 +44,12 @@
           </span>
         </a>
       </nav>
+      <CoreSwitch v-model="switchValue" @click="toggleTheme" />
     </div>
   </header>
 </template>
 
-<script setup>
-const links = [
-  ['Обо мне', '#about'],
-  ['Проекты', '#work'],
-  ['Контакты', '#contacts'],
-]
-</script>
-
-<style lang="postcss">
+<style lang="postcss" scoped>
 .nav-links {
   @apply relative p-0 overflow-hidden flex after:content-[attr(data-after)] after:block after:text-accent-purple after:translate-y-full after:absolute after:left-0 after:transition-inherit;
   transition: transform 0.3s;
