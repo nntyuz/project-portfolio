d9c0637d6f11c4e7429a97bed5102e8eda8999c1
nnuyz <hellonedesing@gmail.com>
2023-08-16 01:42:48 +0300

    add footer

---
+++ components/global/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="section-footer padding-1" id="contacts">
+    <div class="w-container flex flex-col gap-20px">
+      <h3 class="tracking-widest text-14px leading-20px">Контакты</h3>
+      <CoreSocial />
+      <a
+        href="mailto:meri.2008@mail.ru"
+        class="transition-all duration-500 hover:text-accent-purple"
+        >meri.2008@mail.ru</a
+      >
+    </div>
+  </footer>
+</template>
